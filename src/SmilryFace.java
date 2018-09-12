b3c36c1e3e7682c7ebca7ad555b53d54e93f2eff
Jbauer19 <42978498+Jbauer19@users.noreply.github.com>
2018-09-12 14:07:20 -0400

    Add files via upload

---
+++ src/SmilryFace.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @author Josh Bauer
+ * Smiley Face
+ * Period 7
+ *
+ */
+
+public class SmilryFace 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println(" \\ \\ \\ \\ \\ \\");
+		System.out.println("|   *     *    |");
+		System.out.println("|      *       |");
+		System.out.println("|    \\___/     |");
+		System.out.println("\\______________/");
+		
+	}
+
+}
